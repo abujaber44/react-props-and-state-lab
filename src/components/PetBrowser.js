d262843236b54d8f9a5f794e971c09247f04c95a
mjaber <abujaber44@gmail.com>
2021-03-02 13:59:58 -0500

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,7 +4,16 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+
+    const pets = this.props.pets.map((pet, index) => {
+      return <Pet pet={pet} key={index} onAdoptPet={this.props.onAdoptPet} isAdopted={this.props.adoptedPets.includes(pet.id)} />
+    })
+
+    return ( 
+    <div className="ui cards">
+      {pets}
+    </div>
+    );
   }
 }
 
